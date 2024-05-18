bc0991c5ee26b7ce2349578252dded1d3fc2209b
manirathnam5 <manirathnam501@gmail.com>
2024-05-18 11:04:35 +0530

    array example

---
+++ 4.Array/ArrayExample3.java
@@ -0,0 +1,14 @@
+class ArrayExample3{
+
+    public static void main(String[] arg){
+
+        int[] arr = {23,44,55,66,77,99 } ;
+
+        for(int i : arr ){
+
+            System.out.println(i);
+        }
+
+
+    }
+}
