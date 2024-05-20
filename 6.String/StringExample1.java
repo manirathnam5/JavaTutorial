7485c985448aa35c59eaf188b2ecd882b7f78b70
manirathnam5 <manirathnam501@gmail.com>
2024-05-20 12:46:38 +0530

    string examples

---
+++ 6.String/StringExample1.java
@@ -0,0 +1,28 @@
+class StringExample1 {
+
+
+
+     public static void main(String[] arg) {
+
+
+        String s1 = "Google" ;
+
+        String s2 = "Google " ;
+
+
+        System.out.println(s1) ;
+
+        System.out.println(s2) ;
+
+        System.out.println(s1.hashCode());
+
+
+        System.out.println(s2.hashCode()) ;
+
+        System.out.println(s1.equals(s2)) ;
+
+
+
+     }
+    
+}
---
+++ 6.String/StringExample2.java
@@ -0,0 +1,22 @@
+class StringExample2{
+
+
+    public static void main(String[] arg) {
+
+
+        String s1 = "Google" ;
+
+        char ch[] = {'a','b','c','d','e'} ;
+
+        String s2 = new String(ch); 
+
+        String s3 = new String("Facebook");
+        
+        System.out.println(s1) ;
+
+        System.out.println(s2);
+
+
+        System.out.println(s3) ;
+    }
+}
