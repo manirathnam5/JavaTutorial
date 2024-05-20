e977711dda3c98a51053fc99fd29cdf92420d299
manirathnam5 <manirathnam501@gmail.com>
2024-05-20 13:07:48 +0530

    substring example

---
+++ 6.String/SubstringExample1.java
@@ -0,0 +1,14 @@
+class SubstringExample1{
+
+
+    public static void main(String[] arg) {
+
+      String s1 = "Google Opas " ;
+
+
+      String s = s1.substring(3);
+
+      System.out.println(s) ;
+
+    }
+}
