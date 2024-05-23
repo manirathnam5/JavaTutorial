ced639733b9502ffdd4156875ebf397306b8de5c
manirathnam5 <manirathnam501@gmail.com>
2024-05-23 10:01:27 +0530

    substring example

---
+++ 6.String/StringExample4.java
@@ -0,0 +1,15 @@
+class StringExample4{
+
+
+    public static void main(String[] arg){
+
+       String s = "Apartment" ;
+
+       String s2 = s.substring(1);
+
+       System.out.println(s2 ) ;
+
+       
+
+    }
+}
