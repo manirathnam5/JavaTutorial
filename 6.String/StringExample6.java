25c9fedbcf6e02411810ae209c779083675d0581
manirathnam5 <manirathnam501@gmail.com>
2024-05-23 12:31:05 +0530

    string example

---
+++ 6.String/StringExample6.java
@@ -0,0 +1,38 @@
+class StringExample6{
+
+    public static void main(String[] arg){
+
+       // toUppercase() method of String 
+      String str = "welcome to java programming";
+
+
+      String str2 = str.toUpperCase();
+
+      System.out.println(str2) ;
+
+
+     // using toLowerCase() method of String class 
+
+      String company = "DEMOCOMPANY";
+
+      String str3 = company.toLowerCase();
+
+      System.out.println(str3);
+
+      }
+
+
+
+
+      //using trim method 
+
+      String x = "  Hello  world  " ;
+
+      System.out.println(x.trim());
+
+      String y="  Sachin  ";    
+      System.out.println(y);//  Sachin      
+      System.out.println(y.trim());//Sachin 
+      
+   }
+}
