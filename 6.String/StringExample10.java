4549b587e15d27ce779aff0d7fdac4c14227339d
manirathnam5 <manirathnam501@gmail.com>
2024-05-25 23:34:45 +0530

    changes

--- 6.String/StringExample10.java
+++ 6.String/StringExample10.java
@@ -3,4 +3,11 @@ public static void main(String args[]){
 String name="javatpoint";  
 char ch=name.charAt(4);//returns the char value at the 4th index  
 System.out.println(ch);  
+
+
+String name1="javatpoint";  
+//if index is out of range it witll throw StringIndexOutofboundsException
+char ch1=name1.charAt(10);//returns the char value at the 10th index  
+System.out.println(ch1);  
+
 }}  
