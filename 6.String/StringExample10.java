824fcf627d7b8489ea8ad06d620f45d99b9cca88
manirathnam5 <manirathnam501@gmail.com>
2024-05-26 12:56:24 +0530

    removed comments

--- 6.String/StringExample10.java
+++ 6.String/StringExample10.java
@@ -1,7 +1,7 @@
 class StringExample10{  
 public static void main(String args[]){  
 String name="javatpoint";  
-char ch=name.charAt(4);//returns the char value at the 4th index  
+char ch=name.charAt(4);
 System.out.println(ch);  
 
 
