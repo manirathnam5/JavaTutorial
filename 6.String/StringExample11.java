581c4a753ecb69c51d2e3ff4353cb877be3a73d7
manirathnam5 <manirathnam501@gmail.com>
2024-05-27 13:15:03 +0530

    print first & last char

---
+++ 6.String/StringExample11.java
@@ -0,0 +1,17 @@
+class StringExample11{
+
+
+    public static void main(String[] arg){
+
+
+        String str = "cherry in the pie";
+
+        char c = str.charAt(0) ;
+
+        char c2 = str.charAt(str.length()-1) ;
+
+        System.out.println("First char " + c + " Last char " + c2 );
+
+        
+    }
+}
