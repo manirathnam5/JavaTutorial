caddc3bfcc1e23060b03e34b9d2a21c0f04e95a9
manirathnam5 <manirathnam501@gmail.com>
2024-05-28 10:58:57 +0530

    using compareTo method

---
+++ 6.String/StringExample13.java
@@ -0,0 +1,16 @@
+
+// using compareTo method 
+class StringExample13{
+
+    
+     public static void main(String[] arg){
+
+
+          String s1 = "hello" ;
+          String s2 = "aello" ;
+
+          System.out.println(s1.compareTo(s2));
+
+     }
+
+}
