d7d28fd68b61338e059edf2afcb978e384a14ebb
manirathnam5 <manirathnam501@gmail.com>
2024-05-28 11:39:56 +0530

    concat method

--- 6.String/StringExample13.java
+++ 6.String/StringExample13.java
@@ -21,7 +21,35 @@ class StringExample13{
           System.out.println(s3.concat(s4));
 
 
-          
+      //adding spaces using concat method 
+      
+      String s5 = "Hi";
+
+      String s6 = "Programmers";
+
+      String s7 = "of JAVA" ;
+
+    //  String s10 = s5.concat(s6).concat(s7);
+     System.out.println(s5.concat(s6).concat(s7));
+ 
+
+     //System.out.println(s10);
+
+    //add string at beginning using concat
+
+    String s11 = "Developers";
+
+    String s12 = "Hello ".concat(s11);
+
+    System.out.println(s12);
+
+
+    // adding special characters 
+
+     String s14 = s12.concat("@").concat(".com!!");
+
+     System.out.println(s14) ;
+
 
      }
 
