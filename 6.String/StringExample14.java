8c9f6ba11ed764702fc19c7b6fb555f4b8a77eb2
manirathnam5 <manirathnam501@gmail.com>
2024-05-28 12:42:13 +0530

    contains method ex

---
+++ 6.String/StringExample14.java
@@ -0,0 +1,30 @@
+class StringExample14{
+
+    public static void main(String[] arg){
+String s1 = "what do you do now developer";
+
+boolean s = s1.contains("developer") ;
+
+ System.out.println(s);
+        
+ //if you pass null object in contains method it throw nullpointer exception
+try{
+ boolean s2 = s1.contains(null );
+
+ System.out.println(s2);
+}
+catch(Exception e){
+    System.out.println("Please give valid input for contains ");
+}       
+       //small  use case for contains method  
+       
+       String username = "Raj, Suresh , Manoj";
+
+       if(username.contains("Suresh")){
+        System.out.println("user is found ");
+       }else{
+        System.out.println("user not found ");
+
+       }
+    }
+}
