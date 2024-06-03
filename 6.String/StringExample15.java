a333aedc843debf595a3d56ff36b8a5b4069234a
manirathnam5 <manirathnam501@gmail.com>
2024-06-03 22:09:59 +0530

    ex. of trim method

---
+++ 6.String/StringExample15.java
@@ -0,0 +1,15 @@
+class StringExample15{
+
+//ex. of trim method 
+    public static void main(String[] arg) {
+
+      String s1 = "  hello  world  " ;
+ 
+      System.out.println(s1.length());
+
+      String tr = s1.trim() ;
+
+      System.out.println(tr.length());
+
+    }
+}
