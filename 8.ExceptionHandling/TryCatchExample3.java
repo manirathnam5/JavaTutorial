f98b1e87f05a9000f34903d41e410141dcfc9ceb
manirathnam5 <manirathnam501@gmail.com>
2024-06-04 10:40:28 +0530

    examples

--- 8.ExceptionHandling/TryCatchExample3.java
+++ 8.ExceptionHandling/TryCatchExample3.java
@@ -16,6 +16,19 @@ class TryCatchExample3{
 
 
         System.out.println("After try catch block code") ;  // this will executed because exception is handled 
+
+// handling exception using parent class exception 
+
+        try{
+            int a = 100/0 ;
+
+            System.out.println("rest of the code ");
+        }
+
+        catch(Exception ae){
+            System.out.println("Numbers should not be divided by zero ") ;
+        }
+
         
     }
 }
