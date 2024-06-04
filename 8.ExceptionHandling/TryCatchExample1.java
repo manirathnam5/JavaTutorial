04e3211bc9ef5e3de683edb85dfa406eb5b802b9
manirathnam5 <manirathnam501@gmail.com>
2024-06-04 10:33:45 +0530

    try catch ex

---
+++ 8.ExceptionHandling/TryCatchExample1.java
@@ -0,0 +1,13 @@
+class TryCatchExample1{
+
+
+    public static void main(String[] arg ) {
+
+          int a = 100/0 ; //may throw a exception 
+
+
+          System.out.println( "rest of the code ") ;             
+
+    }
+    
+}
---
+++ 8.ExceptionHandling/TryCatchExample3.java
@@ -0,0 +1,21 @@
+class TryCatchExample3{
+
+
+    public static void main(String[] arg) {
+
+
+        try{
+            int a = 100/0 ;
+
+            System.out.println("rest of the code ");
+        }
+
+        catch(ArithmeticException ae){
+            System.out.println("Numbers should not be divided by zero ") ;
+        }
+
+
+        System.out.println("After try catch block code") ;  // this will executed because exception is handled 
+        
+    }
+}
