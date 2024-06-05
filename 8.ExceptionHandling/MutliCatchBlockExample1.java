c7629bc9c8b4baa4088a290930d388c6ef0739ac
manirathnam5 <manirathnam501@gmail.com>
2024-06-05 21:14:05 +0530

    mutlicatch block example

---
+++ 8.ExceptionHandling/MutliCatchBlockExample1.java
@@ -0,0 +1,27 @@
+class MutliCatchBlockExample1{
+
+
+    public static void main(String[] arg) {
+
+          try{
+
+            int[] arr = new int[5] ;
+
+                arr[4]  = 40/0;
+          } 
+
+          catch(ArithmeticException ae) {
+            System.out.println(ae) ;
+          }
+
+          catch(ArrayIndexOutOfBoundsException aie){
+            System.out.println(aie) ;
+          }
+           
+          catch(Exception e){
+            System.out.println(e) ;
+          }
+
+
+    }
+}
