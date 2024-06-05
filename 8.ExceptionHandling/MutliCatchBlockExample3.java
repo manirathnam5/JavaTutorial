2438bb12e43966587f48c7946599eb001940f277
manirathnam5 <manirathnam501@gmail.com>
2024-06-05 21:32:42 +0530

    multicatch block ex

---
+++ 8.ExceptionHandling/MutliCatchBlockExample3.java
@@ -0,0 +1,24 @@
+
+    class MutliCatchBlockExample3 {
+
+    public static void main(String[] arg) {
+
+           
+        try{
+
+           String str = null;
+
+           System.out.println(str.length()) ; // throw null pointer exception 
+        }
+        catch(ArithmeticException ae) {
+            System.out.println(ae) ;
+        }
+        catch(ArrayIndexOutOfBoundsException aie) {
+            System.out.println(aie);
+        }
+        catch(NullPointerException npe){
+            System.out.println(" Object is null ") ;
+        }
+
+    }
+}
