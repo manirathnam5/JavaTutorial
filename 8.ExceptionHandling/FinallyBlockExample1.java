be7e48693a50e4614feb7f94536d5234c7f9d93e
manirathnam5 <manirathnam501@gmail.com>
2024-06-06 11:57:33 +0530

    finally block ex1

---
+++ 8.ExceptionHandling/FinallyBlockExample1.java
@@ -0,0 +1,27 @@
+class FinallyBlockExample1{
+
+   
+    public static void main(String[] arg) {
+       
+        try{
+
+            int[] arr = new int[5] ;
+
+             System.out.println(arr[0]) ;
+        }
+        catch(ArrayIndexOutOfBoundsException aie){
+            System.out.println(aie) ;
+        }
+
+        finally{
+
+            System.out.println("Important code like db connection , resources closing ") ;
+        }
+
+        System.out.println("Rest of the code") ;
+
+
+    }
+
+
+}
