473d23ca697d6b993c02f4d6587a42f27f85fdc5
manrathnam5 <manirathnam501@gmail.com>
2024-06-15 22:25:24 +0530

    inner class ex1

---
+++ 9.InnerClass/InnerClassExample1.java
@@ -0,0 +1,22 @@
+class TestMemberOuter1{  
+ private int data=30;  
+ class Inner{  
+  void msg(){
+    System.out.println("data is "+ data);
+    }  
+ }  
+
+}
+
+class InnerClassExample1{
+
+ public static void main(String args[]){  
+  TestMemberOuter1 obj=new TestMemberOuter1();  
+  
+  TestMemberOuter1.Inner in=obj.new Inner();  
+  
+  in.msg();  
+  
+  }  
+ 
+ }  
