8910df822ed6f9354368b2cb1f3eabfedb0e3584
manrathnam5 <manirathnam501@gmail.com>
2024-06-20 12:47:15 +0530

    nested interface

---
+++ 9.InnerClass/InnerClassExample4.java
@@ -0,0 +1,41 @@
+interface Showable {
+
+
+    void show ();
+
+    interface Messagable{
+
+        void msg();
+
+    }
+
+}
+
+class Testclass implements Showable,Showable.Messagable{
+
+
+    public void msg(){
+        System.out.println("am messaging ") ;
+    }
+
+
+    public void show(){
+        System.out.println("am showing ") ;
+    }
+}
+
+
+class InnerClassExample4{
+
+   public static void main(String[] arg ) {
+ 
+   Showable.Messagable  obj = new Testclass();
+
+   Showable showableObj = new Testclass();
+
+   obj.msg();
+
+   showableObj.show();
+
+     }  
+ }
